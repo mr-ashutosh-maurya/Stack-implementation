58d450ac224da3c665e3c78633b2e86d5b9144de
Ashutosh maurya <ashutoshmauriyacse@gmail.com>
2024-12-02 23:42:28 +0530

    use of inbuilt stack in C++

---
+++ copy_stack_element.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void v(stack<int>&s,stack<int>&result){ //copy element of stack into another stack in same order
+    if(s.empty()) return;  //it is recursion method
+    int curr=s.top();
+    s.pop();
+    v(s,result);
+    result.push(curr);
+}
+int main(){
+    stack<int>s;
+    stack<int>result;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    v(s,result);
+    cout<<result.top();
+}
